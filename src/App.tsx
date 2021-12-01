8132a032ac1d075cca769853acf09b98b1fcc143
Anderson Silva <andssilva.23@gmail.com>
2021-12-01 13:00:02 -0400

    feat: :rocket: instala styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: #8257e6;
+`
 
 export function App() {
   return (
     <div className="App">
-      <h1>Olá Mundo!</h1>
+      <Title>Olá Mundo!</Title>
     </div>
   );
 }
